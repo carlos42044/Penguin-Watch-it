e5be0db0cef7a68785e626fe2b98f06b303f0d9f
carlos42044 <carlos42044@gmail.com>
2016-09-22 13:28:05 -0400

    Test, background change...Rajah(orange)

---
+++ build/chalk/appinfo.auto.c
@@ -0,0 +1,20 @@
+#include "pebble_process_info.h"
+#include "src/resource_ids.auto.h"
+
+const PebbleProcessInfo __pbl_app_info __attribute__ ((section (".pbl_header"))) = {
+  .header = "PBLAPP",
+  .struct_version = { PROCESS_INFO_CURRENT_STRUCT_VERSION_MAJOR, PROCESS_INFO_CURRENT_STRUCT_VERSION_MINOR },
+  .sdk_version = { PROCESS_INFO_CURRENT_SDK_VERSION_MAJOR, PROCESS_INFO_CURRENT_SDK_VERSION_MINOR },
+  .process_version = { 1, 0 },
+  .load_size = 0xb6b6,
+  .offset = 0xb6b6b6b6,
+  .crc = 0xb6b6b6b6,
+  .name = "Penguin Watch It",
+  .company = "carlos42044@gmail.com",
+  .icon_resource_id = DEFAULT_MENU_ICON,
+  .sym_table_addr = 0xA7A7A7A7,
+  .flags = PROCESS_INFO_WATCH_FACE,
+  .num_reloc_entries = 0xdeadcafe,
+  .uuid = { 0x70, 0xD8, 0x00, 0x1C, 0xB6, 0x70, 0x4D, 0xCF, 0x82, 0x97, 0x0D, 0x86, 0x5D, 0xE8, 0x55, 0x11 },
+  .virtual_size = 0xb6b6
+};
