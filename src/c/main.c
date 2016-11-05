ee11e3888edec382055a2f5990e8c29a2efc237b
Carlos <carlos42044@gmail.com>
2016-11-05 02:32:38 -0400

    Fixed background

--- src/c/main.c
+++ src/c/main.c
@@ -7,7 +7,6 @@ static GBitmapSequence *s_sequence;
 static GBitmap *s_bitmap;
 static int count = 0;
 
-// Handles the animation of the apng(Raw binary resource)
 static void timer_handler(void *context) {
   uint32_t next_delay_ms = 1000;
   int timer = 1000;
@@ -16,7 +15,6 @@ static void timer_handler(void *context) {
     // Set the new frame into the BitmapLayer
     bitmap_layer_set_bitmap(s_bitmap_layer, s_bitmap);
     layer_mark_dirty(bitmap_layer_get_layer(s_bitmap_layer));
-    
     
     // Timer for that frame's delay
     app_timer_register(timer, timer_handler, NULL);
@@ -29,17 +27,13 @@ static void update_time() {
   time_t temp = time(NULL);
   struct tm *tick_time = localtime(&temp);
   
-  // write the current hours and minutes into a buffer
   static char s_buffer[8];
   strftime(s_buffer, sizeof(s_buffer), clock_is_24h_style() ?
                                          "%H:%M" : "%I:%M", tick_time);
   
-  // Write the current date into a buffer
   static char s_buffer_date[12]; 
   strftime(s_buffer_date, sizeof(s_buffer_date), "%a, %b %d", tick_time);
-  //APP_LOG(APP_LOG_LEVEL_DEBUG, "s_buffer_date[] value is: %s" , s_buffer_date);
   
-  //Display this time on the TextLayer 
   text_layer_set_text(s_time_layer, s_buffer);
   text_layer_set_text(s_date_layer, s_buffer_date);
 }
@@ -48,28 +42,18 @@ static void tick_handler(struct tm *tick_time, TimeUnits units_changed){
   update_time();
 }
 
-// Function to set all of the text layer's attributes
-
 static void animation(GRect bounds) {
   // Creating the sequence
   s_sequence = gbitmap_sequence_create_with_resource(PBL_IF_COLOR_ELSE(RESOURCE_ID_SCALED_LARGE_COLOR, RESOURCE_ID_SCALED_LARGE_GRAY_SCALE));
   // Create the blank GBitmap using APNG frame size
   GSize frame_size = gbitmap_sequence_get_bitmap_size(s_sequence);
   s_bitmap = gbitmap_create_blank(frame_size, GBitmapFormat8Bit);
-  
-  // Begin the bitmap layer
-  //s_bitmap_layer = bitmap_layer_create(GRect(0, 0, frame_size.w, frame_size.h));
-
-   // s_bitmap_layer = bitmap_layer_create(GRect(0,0, frame_size.w, frame_size.h));
 
   s_bitmap_layer = bitmap_layer_create(GRect(bounds.size.w/2-(bounds.size.w/6), bounds.size.h/32, frame_size.w, frame_size.h));
-  // SET THE LAYER TO COMPOSITING_MODE !!!!!!!!!!
   bitmap_layer_set_compositing_mode(s_bitmap_layer, GCompOpSet); //To make it transparent 
   bitmap_layer_set_bitmap(s_bitmap_layer, s_bitmap);
-  // for app_timer_register
   uint32_t first_delay_ms = 10;
   
-  // Schedule a timer to advance the first frame
   app_timer_register(first_delay_ms, timer_handler, NULL);
       
 }
@@ -90,22 +74,17 @@ static void text_layer_attributes(TextLayer *text_layer, GColor background_color
   text_layer_set_text_color(text_layer, text_color);
   text_layer_set_text_alignment(text_layer, text_alignment);
   text_layer_set_font(s_time_layer, fonts_get_system_font(font));
-  //APP_LOG(APP_LOG_LEVEL_DEBUG, "font_ID value is: %s" , font);
 }
 
 static void main_window_load(Window *window) {
-  // Get information about the window
   Layer *window_layer = window_get_root_layer(window);
   GRect bounds = layer_get_bounds(window_layer);
   
-  // Function that handles the bitmap and animation
   animation(bounds);
   
-  // Gets the bounds of the animation in order to set the time text layer rigt underneath
   Layer *bound_penguin = bitmap_layer_get_layer(s_bitmap_layer);
   GRect bounds_1 = layer_get_bounds(bound_penguin);
 
-  // Create a Time TextLayer with specific bounds
   s_time_layer = text_layer_create(GRect(0, bounds_1.size.h, bounds.size.w, 50)); 
   
   s_date_layer = text_layer_create(GRect(0,bounds.size.h-50, bounds.size.w, 30));
@@ -120,15 +99,12 @@ static void main_window_load(Window *window) {
   // setting the color of the window background
   window_set_background_color(s_main_window, GColorCyan);
   
-  // Add it as a child layer to the Window's root layer
   layer_add_child(window_layer, text_layer_get_layer(s_date_layer));
   layer_add_child(window_layer, text_layer_get_layer(s_time_layer));
   layer_add_child(window_layer, bitmap_layer_get_layer(s_bitmap_layer));
 }
 
-// Destroys whatever was created
 static void main_window_unload(Window *window) {
-  // Destory the text layer
   text_layer_destroy(s_time_layer);
   text_layer_destroy(s_date_layer);
   gbitmap_sequence_destroy(s_sequence);
